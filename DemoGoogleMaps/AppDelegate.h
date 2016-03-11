26c28f455da00b33c30f6c4b540ae8cc8c264c27
Dambert <dambert@damberts-mac.local>
2016-03-11 14:09:38 -0500

    Initial Commit

---
+++ DemoGoogleMaps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoGoogleMaps
+//
+//  Created by Dambert on 3/11/16.
+//  Copyright © 2016 com.starleaks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoGoogleMaps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoGoogleMaps
+//
+//  Created by Dambert on 3/11/16.
+//  Copyright © 2016 com.starleaks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
